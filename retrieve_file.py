9cf9a843baed0042081a8b5e3b0d9891bf574e92
michaeldean22 <63122745+michaeldean22@users.noreply.github.com>
2020-09-17 23:51:19 -0500

    Create retrieve_file.py

---
+++ retrieve_file.py
@@ -0,0 +1,12 @@
+import urllib.request
+from datetime import datetime, date, time, timedelta
+import re
+import os
+
+# this retrieves log file and saves it locally as local_copy.log
+
+URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+LOCAL_PATH = 'local_copy.log'
+
+local_file, headers = urllib.request.urlretrieve(URL_PATH, LOCAL_PATH)
+
