051da71899a7de373d8fadbac1f2a600d49414b2
nicklerma4 <63122433+nicklerma4@users.noreply.github.com>
2020-09-25 13:18:57 -0500

    Update parsing_analyzing.py

--- parsing_analyzing.py
+++ parsing_analyzing.py
@@ -8,6 +8,8 @@ open_file = open("local_copy.log")
 
 # Create counters
 total_requests = 0
+error_count = 0
+errors = []
 month_count = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0}
 day_count = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0}
 fail_count = 0
